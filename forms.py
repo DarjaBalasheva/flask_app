0212fa179c4ba441794805e58aac8cc62cacf010
DarjaBalasheva <balasheva.98@mail.ru>
2023-02-28 12:49:20 +0200

    Super Linter

--- forms.py
+++ forms.py
@@ -1,9 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField)
+from wtforms import StringField
 from wtforms.validators import InputRequired, Length
-import json
- 
+
+
 class CourseForm(FlaskForm):
-   titll = StringField('Введи текст', validators=[InputRequired(),
-                                             Length(max=64)])
+    titll = StringField("Введи текст", validators=[InputRequired(), Length(max=64)])
