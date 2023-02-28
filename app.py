f630cfa77feb55bbb7c64329d4f8892205fc27d9
DarjaBalasheva <balasheva.98@mail.ru>
2023-02-28 11:40:31 +0200

    Black and Flake8

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
-from flask import Flask
-from markupsafe import escape
-from flask import url_for
-from flask import render_template
-from flask import flash, redirect
-from flask import request
-from forms import CourseForm
-import json
 import hashlib
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '3787b2b3631f5e58b18cc7b9dfa5db08b3c8b2d2f1918172'
+from flask import Flask, render_template, request
 
-@app.route('/', methods=['GET', 'POST'])
+from forms import CourseForm
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "3787b2b3631f5e58b18cc7b9dfa5db08b3c8b2d2f1918172"
+
+
+@app.route("/", methods=["GET", "POST"])
 def form_hashed_message():
     form = CourseForm()
     if request.method == "POST":
-        hashed_message = hashlib.md5(request.form.get('titll').encode()).hexdigest()
-        return render_template('base.html',
-                                form=form,
-                                hashed_message=hashed_message)
-    else: return render_template('base.html',
-                                form=form)
+        hashed_message = hashlib.md5(request.form.get("titll").encode()).hexdigest()
+        return render_template("base.html", form=form, hashed_message=hashed_message)
+
+    else:
+        return render_template("base.html", form=form)
 
 
-@app.route('/json', methods=['GET'])
+@app.route("/json", methods=["GET"])
 def json_hashed_message():
-    hashed_message = 'Hi'
+    #    data = request.json
+    hashed_message = {
+        "title": "Hi",
+        "name": {"first_name": "Dasha", "last_name": "Balasheva"},
+        "age": 25,
+        "city": "Narva",
+    }
+
     return {
-        'hashed_message' : hashed_message
+        "first_name": hashed_message["name"]["first_name"],
+        "last_name": hashed_message["name"]["last_name"],
+        "age": hashed_message["age"],
     }
