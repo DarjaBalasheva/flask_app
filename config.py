ba09a651888bed091b1e2d31ac689639e4c4fa5b
DarjaBalasheva <balasheva.98@mail.ru>
2023-03-09 16:29:04 +0200

    base

--- config.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseSettings
-
-
-class DefaultSetting(BaseSettings):
-    SECRET_KEY: str = "3787b2b3631f5e58b18cc7b9dfa5db08b3c8b2d2f1918172"
--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField)
-from wtforms.validators import InputRequired, Length
-import json
- 
-class CourseForm(FlaskForm):
-   titll = StringField('Введи текст', validators=[InputRequired(),
-                                             Length(max=64)])
